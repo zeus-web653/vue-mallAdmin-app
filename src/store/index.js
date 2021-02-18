4e48915f48ef382402e8328fd7773288a068f4c5
zeus <2310115120@qq.com>
2021-02-18 12:24:04 +0800

    8-2.整体布局代码优化‘

--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,18 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    // 用于菜单切换的闭合状态 false:不闭合 true:闭合
+    collapsed: false,
   },
   mutations: {
+    changeCollapsed(state) {
+      state.collapsed = !state.collapsed;
+    },
   },
   actions: {
+    changeCollapsed({ commit }) {
+      commit('changeCollapsed');
+    },
   },
-  modules: {
-  },
+  modules: {},
 });
---
+++ src/views/layout/components/sliderNav.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="main-header">
+    <a-button
+      type="primary"
+      style="margin-bottom: 16px"
+      @click="toggleCollapsed"
+    >
+      <a-icon :type="$store.state.collapsed ? 'menu-unfold' : 'menu-fold'" />
+    </a-button>
+    <div class="breadcrumb">
+      <a-breadcrumb>
+        <a-breadcrumb-item>首页</a-breadcrumb-item>
+        <a-breadcrumb-item><a href="">统计</a></a-breadcrumb-item>
+      </a-breadcrumb>
+    </div>
+    <ul class="user-info">
+      <li>欢迎，zeus <a-icon type="down" /></li>
+      <li>退出</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+
+  methods: {
+    toggleCollapsed() {
+      this.$store.dispatch('changeCollapsed');
+    },
+  },
+};
+</script>
