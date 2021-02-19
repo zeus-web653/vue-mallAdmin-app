2d092aa74e267a30361999d3f2d1ea8a5254a353
zeus <2310115120@qq.com>
2021-02-19 19:47:29 +0800

    10-2.菜单过滤展示‘

--- src/views/layout/components/menu.vue
+++ src/views/layout/components/menu.vue
@@ -1,30 +1,38 @@
 <template>
   <div class="menu-list">
-    <a-menu :default-selected-keys="['1']"
-     :default-open-keys="['sub1']" mode="inline" theme="dark"
-      :inline-collapsed="$store.state.collapsed">
-      <a-sub-menu v-for="route in $store.state.menuRoutes" :key="route.name">
-        <span slot="title">
-          <a-icon type="mail" />
-          <span>{{route.meta.title}}</span>
-        </span>
-        <a-menu-item v-for="child in route.children" :key="child.name">
-          {{child.meta.title}}
-        </a-menu-item>
-
-      </a-sub-menu>
+    <a-menu
+      :default-selected-keys="[
+        $router.currentRoute.matched[1] ? $router.currentRoute.matched[0].name : ''
+      ]"
+      :default-open-keys="[$router.currentRoute.matched[0].name]"
+      mode="inline"
+      theme="dark"
+      :inline-collapsed="$store.state.collapsed"
+    >
+      <template v-for="route in $store.state.menuRoutes">
+        <a-sub-menu v-if="!route.meta.hidden" :key="route.name">
+          <span slot="title">
+            <a-icon :type="route.meta.icon" />
+            <span>{{ route.meta.title }}</span>
+          </span>
+          <a-menu-item v-for="child in route.children" :key="child.name">
+            <router-link :to="{ name: child.name }">
+              <a-icon :type="child.meta.icon" />{{ child.meta.title }}</router-link
+            >
+          </a-menu-item>
+        </a-sub-menu>
+      </template>
     </a-menu>
   </div>
 </template>
 <style lang="less">
-  .menu-list {
-    width: 200px;
-    position: fixed;
+.menu-list {
+  width: 200px;
+  position: fixed;
+  height: 100%;
+
+  .ant-menu {
     height: 100%;
-
-    .ant-menu {
-      height: 100%;
-    }
   }
-
+}
 </style>
